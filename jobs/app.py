b37b99afa33aceb6412a9a586fe04477cf9f9364
Satvik Vishnubhatta <satvik.2692@gmail.com>
2021-01-08 16:40:10 -0800

    Completed Module 2

--- jobs/app.py
+++ jobs/app.py
@@ -3,10 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/jobs')
 def jobs():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
